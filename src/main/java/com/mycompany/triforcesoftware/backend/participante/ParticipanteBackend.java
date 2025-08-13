d7ef97c23b6592f0f5e0233db7d73bbf2c286da8
HuronClinch <onljsd@gmail.com>
2025-08-13 12:30:12 -0600

    Agrega funcion comunicar controlarParticipante y Opcion de listado incritos en ControladorParticipante

--- src/main/java/com/mycompany/triforcesoftware/backend/participante/ParticipanteBackend.java
+++ src/main/java/com/mycompany/triforcesoftware/backend/participante/ParticipanteBackend.java
@@ -5,7 +5,11 @@
 package com.mycompany.triforcesoftware.backend.participante;
 
 import com.mycompany.triforcesoftware.Atajos;
+import com.mycompany.triforcesoftware.controladores.ControladorParticipante;
 import com.mycompany.triforcesoftware.frontend.participante.ParticipantePanel;
+import com.mycompany.triforcesoftware.modelos.participante.Participante;
+import java.util.LinkedList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -31,23 +35,41 @@ public class ParticipanteBackend {
     }
 
     public void comprobarDatos() {//Comprobar si los datos esta ingresados correctamente
-        String dato1 = PANEL.getCampo1().getText();
-        String dato2 = PANEL.getCampo2().getItemAt(PANEL.getCampo2().getSelectedIndex());
-        String dato3 = PANEL.getCampo3().getText();
-        String dato4 = PANEL.getCampo4().getText();
+        String nombre = PANEL.getCampo1().getText();
+        String tipoParticipante = PANEL.getCampo2().getItemAt(PANEL.getCampo2().getSelectedIndex());
+        String istitucion = PANEL.getCampo3().getText();
+        String correo = PANEL.getCampo4().getText();
 
         Atajos atajos = new Atajos();
-        if (atajos.comprobarTexto(dato1, "Nombre completo", 45)
-                && (dato2.equals(TIPO[0]) || dato2.equals(TIPO[1]) || dato2.equals(TIPO[2]))
-                && atajos.comprobarTexto(dato3, "Institucion de procedencia", 150)
-                && atajos.comprobarTexto(dato4, "Correo electronico", 45)) {//Comprobar si los valeres cumplen con los requisitos
-            System.out.println("Correcto");
-            /*
-            guardar informacion participante
-            hacer validacion de que no exita otro con el mismo nombre
-             */
+        if (atajos.comprobarTexto(nombre, "Nombre completo", 45)
+                && (tipoParticipante.equals(TIPO[0]) || tipoParticipante.equals(TIPO[1]) || tipoParticipante.equals(TIPO[2]))
+                && atajos.comprobarTexto(istitucion, "Institucion de procedencia", 150)
+                && atajos.comprobarTexto(correo, "Correo electronico", 45)) {//Comprobar si los valeres cumplen con los requisitos
+
+            ControladorParticipante controlador = new ControladorParticipante();
+
+            if (VerificarCorreo(controlador.getLista(), correo)) {//Si no existe el correo ingresado
+                controlador.nuevoParticipante(nombre, tipoParticipante, istitucion, correo);//Ingresar participante en base 
+                JOptionPane.showMessageDialog(null, "Participante creado exitosamente");
+            }
         } else {
             System.out.println("dato incorrecto");
         }
+    }
+
+    private boolean VerificarCorreo(LinkedList<Participante> lista, String correo) {//Comprobar si ya existe el correo ingresado 
+        for (Participante participante : lista) {
+            if (participante.getCorreoElectronico().equals(correo)) {//Comprobar si ya esta ingresado el correo
+                JOptionPane.showMessageDialog(null, "Correo no valido, ya existe");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void LimpiarDatos() {//Limpiar campos de texto
+        PANEL.getCampo1().setText("");
+        PANEL.getCampo3().setText("");
+        PANEL.getCampo4().setText("");
     }
 }
--- src/main/java/com/mycompany/triforcesoftware/controladores/ControladorParticipante.java
+++ src/main/java/com/mycompany/triforcesoftware/controladores/ControladorParticipante.java
@@ -9,6 +9,7 @@ import com.mycompany.triforcesoftware.modelos.participante.ParticipanteCRUD;
 import com.mycompany.triforcesoftware.utileria.Connect;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.LinkedList;
 
 /**
  *
@@ -16,22 +17,33 @@ import java.sql.SQLException;
  */
 public class ControladorParticipante {
 
-    private Connection connection;
+    private Connection CONNECTION;
+    private ParticipanteCRUD CRUD;
+    private LinkedList<Participante> lista;
 
     public ControladorParticipante() {
-        connection = (Connection) new Connect();
+        Connect conceccion = new Connect();
+        CONNECTION = conceccion.connect();//Crear conceccion con base de datos 
+        CRUD = new ParticipanteCRUD();//
+        ListadoParticipantes();//Obtener el listado de las personas incritas
     }
 
     public void nuevoParticipante(String campo1, String campo2, String campo3, String campo4) {//Crear participante
         try {
             Participante participante = new Participante(campo1, campo2, campo3, campo4);//Crear Personaje
-
-            ParticipanteCRUD crearParicipante = new ParticipanteCRUD();
-            crearParicipante.crear(connection, participante);//Agregar participante
-            connection.close();
+            CRUD.crear(CONNECTION, participante);//Agregar participante
+            CONNECTION.close();
         } catch (SQLException e) {
-            System.out.println("Eroro en crear participante");
+            System.out.println("Eror en crear participante");
             e.printStackTrace();
         }
+    }
+
+    private void ListadoParticipantes() {//Obtener los datos de los participantes
+        lista = CRUD.leerTodos(CONNECTION);
+    }
+
+    public LinkedList<Participante> getLista() {
+        return lista;
     }
 }
