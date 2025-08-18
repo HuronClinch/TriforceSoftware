d0f16eaf96b1c4cef9a1d6818ba60eeb79b2a90d
HuronClinch <onljsd@gmail.com>
2025-08-18 17:06:39 -0600

    Actualizacion backend participante

--- src/main/java/com/mycompany/triforcesoftware/backend/participante/ParticipanteBackend.java
+++ src/main/java/com/mycompany/triforcesoftware/backend/participante/ParticipanteBackend.java
@@ -7,7 +7,9 @@ package com.mycompany.triforcesoftware.backend.participante;
 import com.mycompany.triforcesoftware.Atajos;
 import com.mycompany.triforcesoftware.controladores.ControladorParticipante;
 import com.mycompany.triforcesoftware.frontend.participante.ParticipantePanel;
+import com.mycompany.triforcesoftware.modelos.inscripcion.Inscripcion;
 import com.mycompany.triforcesoftware.modelos.participante.Participante;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import javax.swing.JOptionPane;
 
@@ -25,6 +27,10 @@ public class ParticipanteBackend {
         this.PANEL = PANEL;
     }
 
+    public ParticipanteBackend() {
+        this.PANEL = null;
+    }
+
     public void datos() {//Llenar co comboBox con las valores definidos
         PANEL.getCampo2().removeAll();
         PANEL.getCampo2().repaint();
@@ -37,28 +43,43 @@ public class ParticipanteBackend {
     public void comprobarDatos() {//Comprobar si los datos esta ingresados correctamente
         String nombre = PANEL.getCampo1().getText();
         String tipoParticipante = PANEL.getCampo2().getItemAt(PANEL.getCampo2().getSelectedIndex());
-        String istitucion = PANEL.getCampo3().getText();
+        String institucion = PANEL.getCampo3().getText();
         String correo = PANEL.getCampo4().getText();
 
-        Atajos atajos = new Atajos();
-        if (atajos.comprobarTexto(nombre, "Nombre completo", 45)
-                && (tipoParticipante.equals(TIPO[0]) || tipoParticipante.equals(TIPO[1]) || tipoParticipante.equals(TIPO[2]))
-                && atajos.comprobarTexto(istitucion, "Institucion de procedencia", 150)
-                && atajos.comprobarTexto(correo, "Correo electronico", 45)) {//Comprobar si los valeres cumplen con los requisitos
-
-            ControladorParticipante controlador = new ControladorParticipante();
-
-            if (VerificarCorreo(controlador.getLista(), correo)) {//Si no existe el correo ingresado
-                controlador.nuevoParticipante(nombre, tipoParticipante, istitucion, correo);//Ingresar participante en base 
-                JOptionPane.showMessageDialog(null, "Participante creado exitosamente");
-            }
+        Participante participante = new Participante(nombre, tipoParticipante, institucion, correo);//Crear Personaje
+        if (datosValidos(participante)) {
+            LimpiarDatos();
         }
+
     }
 
-    private boolean VerificarCorreo(LinkedList<Participante> lista, String correo) {//Comprobar si ya existe el correo ingresado 
+    public boolean datosValidos(Participante participante) {
+        try {
+            Atajos atajos = new Atajos();
+            if (atajos.comprobarTexto(participante.getNombreCompleto(), "Nombre completo", 45)
+                    && (participante.getTipoParticipante().equals(TIPO[0]) || participante.getTipoParticipante().equals(TIPO[1]) || participante.getTipoParticipante().equals(TIPO[2]))
+                    && atajos.comprobarTexto(participante.getInstitucionProcedencia(), "Institucion de procedencia", 150)
+                    && atajos.comprobarTexto(participante.getCorreoElectronico(), "Correo electronico", 45)) {//Comprobar si los valeres cumplen con los requisitos
+
+                ControladorParticipante controlador = new ControladorParticipante();
+
+                if (verificarCorreo(controlador.getLista(), participante.getCorreoElectronico())) {//Si no existe el correo ingresado
+                    controlador.nuevoParticipante(participante);//Ingresar participante en base
+                    JOptionPane.showMessageDialog(null, "Participante creado exitosamente");
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al Validar datos participante");
+            e.printStackTrace(System.out);
+        }
+        return false;
+    }
+
+    private boolean verificarCorreo(LinkedList<Participante> lista, String correo) {//Comprobar si ya existe el correo ingresado 
         for (Participante participante : lista) {
             if (participante.getCorreoElectronico().equals(correo)) {//Comprobar si ya esta ingresado el correo
-                JOptionPane.showMessageDialog(null, "Correo no valido, ya existe");
+                JOptionPane.showMessageDialog(null, "Correo no valido para registro de participante, ya existe");
                 return false;
             }
         }
